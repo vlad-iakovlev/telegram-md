c871b7584aefe20dfc76c9a4d60bde54ee86674f
Vladislav Iakovlev <vlad309523@gmail.com>
2026-09-12 02:14:23 +0200

    chore(lint): run prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,12 @@ import eslintConfig from '@vlad-iakovlev/eslint-config'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig(
-  globalIgnores(['coverage', '.vitest', 'packages/*/dist', 'prettier.config.js']),
+  globalIgnores([
+    'coverage',
+    '.vitest',
+    'packages/*/dist',
+    'prettier.config.js',
+  ]),
   eslintConfig.node,
   {
     languageOptions: {
